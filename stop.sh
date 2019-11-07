2da6a6c4f95a502464fb67f221fb2ad1d91b98ab
Diones Gomes <diones.zack.dz@gmail.com>
2019-11-07 20:06:05 -0300

    Adicionando script docker para parar os containers

---
+++ stop.sh
@@ -0,0 +1,7 @@
+dokcer stop app             #Parando o container com o nome 'app'
+docker rm app               #Removendo o container com o nome 'app'
+docker rmi -f dac-jpa/app   #Removendo a imagen
+
+docker stop bd            #Parando o container com o nome 'bd'
+docker rm bd              #Removendo o container com o nome 'bd'
+docker rmi -f dac-jpa/bd  #Removendo a magen
