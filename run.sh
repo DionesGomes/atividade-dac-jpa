81381aa3fe23786cb2dc03a260107ba01f70454e
Diones Gomes <diones.zack.dz@gmail.com>
2019-11-08 10:28:36 -0300

    Update scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-docker build -t dac-jpa/bd ./postgres  # Build Dockerfile
-docker run -p 5433:5432 --name bd -d dac-jpa/bd
+docker build -t dac-jpa/postgres ./postgres  # Build Dockerfile
+docker run -p 5433:5432 --name postgres -d dac-jpa/postgres
 
 mvn clean package                      #Constuir o projeto.
 
@@ -7,4 +7,4 @@ chmod -R 777 ./target
 docker build -t dac-jpa/app .          # Build Dockerfile
 docker run -p 8080:8080 --name app --link bd:host-container dac-jpa/app
 docker logs -f app
-docker logs -f bd
+docker logs -f postgres
